ad023ac867f88d66116ed75aff97ff304fc1f20f
Tello <pateltanaka22@live.com>
2021-08-29 20:32:09 +0200

    added  post users

--- src/app/views/controllers/admin/Users/users-form/users-form.component.ts
+++ src/app/views/controllers/admin/Users/users-form/users-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { UserService } from 'src/app/tools/services';
+import { AlertService, GroupService, UserService } from 'src/app/tools/services';
 
 @Component({
   selector: 'app-users-form',
@@ -17,19 +17,28 @@ export class UsersFormComponent implements OnInit {
   returnUrl: string;
   error = '';
   roles=[];
-  branches=[];
+  groups=[];
   ready:boolean = false
-  constructor(public dialog : MatDialog,private userServices:UserService ,   public snackBar: MatSnackBar ) { }
+  constructor(public dialog : MatDialog,private userServices:UserService , private alertServices:AlertService,  public snackBar: MatSnackBar , private groupService:GroupService ) { }
 
 
 
   ngOnInit() {
         this.createUserForm();
-
+        this.getGroups()
   
  
   }
 
+
+  private getGroups (){
+
+
+     this.groupService.getAllGroups().subscribe((resp:any)=>{ 
+      this.groups =  resp
+      this.ready =  true
+     })
+  }
 
 
 
@@ -39,13 +48,15 @@ export class UsersFormComponent implements OnInit {
   createUserForm(){
 
     this.userForm = new FormGroup({
-      name: new FormControl('', [Validators.required]),
-      ec: new FormControl('', [Validators.required]),
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required]),
-      branchId: new FormControl('', [Validators.required]),
-      role: new FormControl('', [Validators.required]),
-
-
+      groupId: new FormControl('', [Validators.required]),
+      ownerId: new FormControl('', [Validators.required]),
+      phoneNumber:new FormControl(''),
+      initials: new FormControl(''),
+      username: new FormControl(''),
+      portalClient:new FormControl('')
 
     });
 
@@ -57,6 +68,13 @@ export class UsersFormComponent implements OnInit {
 
 
   async onSubmit() {
+this.userServices.post(this.userForm.value).subscribe(resp=>{
+   this.alertServices.success('Done')
+} ,
+err=>{
+
+   this.alertServices.error(err.message)
+})
 
 
 
